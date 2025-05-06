b6b3d4e790402efe61a1cc1654cc568ca0baabe5
Agent47.Guru <83501157+GuruSaran47@users.noreply.github.com>
2025-05-06 23:15:24 +0530

    Update main_file.ino

--- main_file.ino
+++ main_file.ino
@@ -9,12 +9,12 @@
 
 static uint8 gsu8_qmc_settings;
 static uint8 gsu8_control2_settings;
-
+static uint8 gsu8_control_reg2_status;
+static uint8 gsu8_status_value;
 static t_Axis_Data axis_values;
 
 static sint16 gss16_Temperature;
-
-/*
+/*---------------------------------------------------------------------------------
 *   Func: qmc_5883l_init(uint8 mode, uint8 odr, uint8 scale, uint8 osr)
 *   Description: used to set the settings we have set in the sensor for operation
 */
@@ -31,8 +31,7 @@ void qmc_5883l_init(uint8 mode, uint8 odr, uint8 scale, uint8 osr){
   Wire.write(gsu8_qmc_settings);
   Wire.endTransmission();
 }
-
-/*
+/*---------------------------------------------------------------------------------
 *   Func: debug_settings(void)
 *   Description: used for reading the settings we have set in the sensor for operation
 */
@@ -55,14 +54,13 @@ void debug_settings(void){
   delay(10);
 
   Wire.requestFrom(QMC_5883L_ADDR,ONE_BYTE);
-  msu8_qmc_setting = ((Wire.read())); 
+  msu8_qmc_setting = Wire.read(); 
 #ifdef DEBUG
   Serial.print("CONTROL B: ");
   Serial.println(msu8_qmc_setting);  
 #endif
 }
-
-/*
+/*---------------------------------------------------------------------------------
 *   Func: set_control_reg2(uint8 set_reset, uint8 set_rol_pointer, uint8 set_int)
 *   Description: used for reading the settings we have set in the sensor for operation
 */
@@ -75,8 +73,20 @@ void set_control_reg2(uint8 set_reset, uint8 set_rol_pointer, uint8 set_int){
   Wire.write(QMC_CONTROL_02);
   Wire.write(gsu8_control2_settings);
   Wire.endTransmission();
+  delay(10);
+
 }
-/*
+/*---------------------------------------------------------------------------------
+*   Func: uint8 read_control_reg2(void)
+*   Description: read the second control register
+*/
+uint8 read_control_reg2(void){
+  Wire.requestFrom(QMC_CONTROL_02,ONE_BYTE);
+  gsu8_control_reg2_status = Wire.read();
+
+  return gsu8_control_reg2_status;
+}
+/*---------------------------------------------------------------------------------
 *   Func: uint8 debug_status(void)
 *   This function returns the status register value
 */
@@ -96,7 +106,7 @@ uint8 debug_status(void){
 #endif
   return qmc_status;
 }
-/*
+/*---------------------------------------------------------------------------------
 *   Func: voidread_Xaxis(void)
 *   Description: used for reading the values measured on X axis
 */
@@ -108,27 +118,24 @@ void read_Allaxis(void){
     Wire.endTransmission();
     delay(10);
 
-    Wire.requestFrom(QMC_5883L_ADDR,EIGHT_BYTES);
+    Wire.requestFrom(QMC_5883L_ADDR,SIX_BYTES);
     uint8 x_lsb = Wire.read();
     uint8 x_msb = Wire.read();
     uint8 y_lsb = Wire.read();
     uint8 y_msb = Wire.read();
     uint8 z_lsb = Wire.read();
     uint8 z_msb = Wire.read();
-    sint8 t_lsb = Wire.read();
-    sint8 t_msb = Wire.read();
 
     axis_values.xAxis = (x_msb<<8)|x_lsb;
     axis_values.yAxis = (y_msb<<8)|y_lsb;
     axis_values.zAxis = (z_msb<<8)|z_lsb;
-    gss16_Temperature = (t_msb<<8)|t_lsb;
   }
   else{
     Serial.println("Check the sensor settings : Refer to qmc_5883l_init() settings");
+    Serial.println(sensor_status);
   }
 }
-
-/* 
+/*---------------------------------------------------------------------------------
 * func :        void debug_values(void)
 * Description:  This function can be used to initialize ones 
 */
@@ -136,8 +143,36 @@ void debug_all(void){
     Serial.print("Temperature: ");Serial.println(gss16_Temperature);
     Serial.print("X: ");Serial.print(axis_values.xAxis);Serial.print(" Y: ");Serial.print(axis_values.yAxis);Serial.print(" Z: ");Serial.println(axis_values.zAxis);
 }
+/*---------------------------------------------------------------------------------
+* Func: void soft_reset(void)
+* Description: function for performing soft reset of the sensor
+*/
+void soft_reset(void){
+  Wire.beginTransmission(QMC_5883L_ADDR);
+  Wire.write(QMC_CONTROL_02);
+  Wire.write(SOFT_RESET);
+  Wire.endTransmission();
+  delay(50);
+}
 
-/* 
+/*---------------------------------------------------------------------------------
+* func :        float get_heading(void)
+* Description:  This function can be used to read the heading values
+*/
+float get_heading(void){
+  float heading = atan2((float)axis_values.yAxis,(float)axis_values.xAxis);
+  heading = heading * (180/M_PI);
+  heading += 0.22;
+
+  if(heading>360){
+    heading -= 360;
+  }
+  if(heading<0){
+    heading += 360;
+  }
+  return heading;
+}
+/*---------------------------------------------------------------------------------
 * func :        void setup(void)
 * Description:  This function can be used to initialize ones 
 */
@@ -146,15 +181,14 @@ void setup() {
   Wire.begin();
   Wire.setClock(Clock_400KHz);
   /* Here we have kept everything at the basic settings, with good data rate but high sensitivity for compassing applications */
-  qmc_5883l_init(MODE_CONTINUOUS,ODR_100Hz,SCALE_2G,OSR_256);
+  qmc_5883l_init(MODE_CONTINUOUS,ODR_100Hz,SCALE_8G,OSR_256);
   Serial.begin(115200);
 }
-
-/* 
+/*---------------------------------------------------------------------------------
 * func :        void loop(void)
 * Description:  This function runs in loops the usual sense 
 */
 void loop() {
-  debug_all();
   read_Allaxis();
+  Serial.println(get_heading());
 }
